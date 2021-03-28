99974f63bb9e8773b67c34e192d43e05a0269f9b
Robert Guerra <robertjguerra@yahoo.com>
2021-03-28 02:18:35 -0700

    Created Scrape for Microcenter Parts, Refactor

---
+++ parts_info.py
@@ -0,0 +1,37 @@
+newegg_parts = {
+        'gpu': {'link': 'https://www.newegg.com/Desktop-Graphics-Cards/SubCategory/ID-48?Tid=7709',
+                'ratio': {'gaming': .34, 'productivity': .38}},
+        'cpu': {'link': 'https://www.newegg.com/CPUs-Processors/Category/ID-34',
+                'ratio': {'gaming': .17, 'productivity': .20}},
+        'ram': {'link': 'https://www.newegg.com/Desktop-Memory/SubCategory/ID-147?Tid=7611',
+                'ratio': {'gaming': .06, 'productivity': .06}},
+        'ssd': {'link': 'https://www.newegg.com/p/pl?Submit=StoreIM&Category=119&Depa=1',
+                'ratio': {'gaming': .1, 'productivity': .10}},
+        'hdd': {'link': 'https://www.newegg.com/p/pl?Submit=StoreIM&Category=15&Order=1',
+                'ratio': {'gaming': .07, 'productivity': .07}},
+        'psu': {'link': 'https://www.newegg.com/Power-Supplies/Category/ID-32?Tid=6656',
+                'ratio': {'gaming': .07, 'productivity': .07}},
+        'mobo': {'link': None,
+                'ratio': {'gaming': .11, 'productivity': .09}},
+        'cases': {'link': 'https://www.newegg.com/Computer-Cases/Category/ID-9?Tid=6644',
+                'ratio': {'gaming': .08, 'productivity': .08}}
+    }
+
+microcenter_parts = {
+        'gpu': {'link': 'https://www.microcenter.com/search/search_results.aspx?N=4294966937&NTK=all&sortby=match&rpp=96',
+                'ratio': {'gaming': .34, 'productivity': .38}},
+        'cpu': {'link': 'https://www.microcenter.com/search/search_results.aspx?N=4294966995&Ntk=all&sortby=match&myStore=true',
+                'ratio': {'gaming': .17, 'productivity': .20}},
+        'ram': {'link': 'https://www.microcenter.com/search/search_results.aspx?Ntk=all&sortby=match&N=4294966965&myStore=false',
+                'ratio': {'gaming': .06, 'productivity': .06}},
+        'ssd': {'link': 'https://www.microcenter.com/category/4294945779/ssd-solid-state-drives',
+                'ratio': {'gaming': .1, 'productivity': .10}},
+        'hdd': {'link': 'https://www.microcenter.com/category/4294945772/internal-hard-drives',
+                'ratio': {'gaming': .07, 'productivity': .07}},
+        'psu': {'link': 'https://www.microcenter.com/search/search_results.aspx?N=&cat=&Ntt=power+supply&searchButton=search',
+                'ratio': {'gaming': .07, 'productivity': .07}},
+        'mobo': {'link': None,
+                'ratio': {'gaming': .11, 'productivity': .09}},
+        'cases': {'link': 'https://www.microcenter.com/category/4294964318/desktop-cases',
+                'ratio': {'gaming': .08, 'productivity': .08}}
+    }
---
+++ scraper.py
@@ -0,0 +1,58 @@
+import pandas as pd
+
+
+def scrape(tags, site, price, ratio):
+
+    df = pd.DataFrame()
+
+    # LOOPING THROUGH TAGS AND EXTRACTING PERTINENT INFORMATION
+    for tag in tags:
+        # USING RATIO AND TOTAL PRICE TO CALCULATE ALLOTMENT FOR PC PART
+        price_point = price * ratio
+
+        # CREATING KEY-VALUE PAIRING TO HOLD ATTRIBUTES OF PARTS
+        obj = {}
+
+        if site is 'newegg':
+            try:
+                tag_name = tag.a.img.get("title")
+                tag_img = tag.a.img.get("src")
+                tag_href = tag.a.get("href")
+                tag_num_of_ratings = \
+                    tag.find("span", {"class": "item-rating-num"}).text.replace('(', '').replace(')', '').replace(',', '')
+                tag_dollars = tag.find("li", {"class": "price-current"}).strong.text
+                tag_cents = tag.find("li", {"class": "price-current"}).sup.text
+                tag_rating = tag.find("a", {"class": "item-rating"}).get("title")[-1]
+            except AttributeError:
+                continue
+
+        elif site is 'microcenter':
+            try:
+                tag_name = tag.find("a", {"class": "image"}).get("data-name")
+                tag_img = tag.find("img", {"class": "SearchResultProductImage"}).get("src")
+                tag_href = tag.find("a", {"class": "image"}).get("href")
+                tag_num_of_ratings = tag.find("div", {"class": "ratingstars"}).div.span.text.replace('(', '').replace(')', '').replace(',', '')
+                tag_price = tag.find("a", {"class": "image"}).get("data-price")
+                tag_rating = tag.find("img", {"class": "imgReviews"}).get("alt")
+            except AttributeError:
+                continue
+
+        # APPENDING ALL SCRAPED ATTRIBUTES TO A DICTIONARY
+        obj["name"] = tag_name
+        if site is 'newegg':
+            obj["price"] = float(f'{tag_dollars}{tag_cents}'.replace(',', ''))
+        elif site is 'microcenter':
+            obj["price"] = float(tag_price)
+        obj["href"] = f'{tag_href}'
+        obj["rating"] = float(tag_rating[0])
+        obj["image"] = tag_img
+        try:
+            obj["num_ratings"] = int(tag_num_of_ratings.replace('reviews', '').lstrip(' ').rstrip(' '))
+        except ValueError:
+            obj["num_ratings"] = int(tag_num_of_ratings.replace('review', '').lstrip(' ').rstrip(' '))
+
+        # APPEND ONLY THOSE PC PARTS THAT FALL UNDER OUR ALLOTTED AMOUNT
+        if obj["price"] <= price_point:
+            df = df.append(obj, ignore_index=True)
+
+    return df
